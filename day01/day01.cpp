4a14d1d16de64d5d74eea4df7a591641f86e40fc
Alexander Jenkins <alex@alexjenkins.dev>
2024-01-01 13:04:58 -0500

    Completed Day 01 Part 02.

--- day01/day01.cpp
+++ day01/day01.cpp
@@ -19,7 +19,7 @@ void day01::part01() {
         std::vector<int> list;
 
         // linear search thru the line to find all numbers, when found push to the list
-        for (char c : line)
+        for (char c: line)
             if (isnumber(c)) list.push_back(c - '0');
 
         // add the calibration value of the line to the sum of all calibration values
@@ -37,23 +37,86 @@ void day01::part02() {
     std::string line;
     int sum = 0;
 
+    // loop through every line in the input file
     while (std::getline(inputFile, line)) {
-        std::vector<int> numbers;
 
-        // loop over each char
+        // empty list to hold all the numbers that appear in the string
+        std::vector<int> list;
+
+        // linear search thru the line to find all numbers, when found push to the list
         for (int i = 0; i < line.length(); i++) {
+            // get the current character
+            char c = line.at(i);
+            int numLeft = line.length() - i;
 
-            // look forward
-            for (int j = 0; (j < line.length() && j <= 5); j++) {
+            // if the character is a number, add to the list and go to the next iteration of the loop
+            if (isnumber(c)) {
+                list.push_back(c - '0');
+                continue;
+            }
 
-                if (isdigit(line[j])) {
-                    numbers.push_back(line[j] - '0');
-                    break;
+                // if not, check to see if the next few characters spell out a number
+            else {
+                // check if this letter is even the start of a valid word
+                switch (c) {
+                    case 'o':
+                        if (numLeft >= 2) {
+                            std::string slice = line.substr(i, 3);
+                            if (slice == "one") list.push_back(1);
+                        }
+                        break;
+
+                    case 't':
+                        if (numLeft >= 2) {
+                            std::string slice = line.substr(i, 3);
+                            std::string slice2 = line.substr(i, 5);
+                            if (slice == "two") list.push_back(2);
+                            if (slice2 == "three") list.push_back(3);
+                        }
+                        break;
+
+                    case 'f':
+                        if (numLeft >= 4) {
+                            std::string slice = line.substr(i, 4);
+                            if (slice == "four") list.push_back(4);
+                            if (slice == "five") list.push_back(5);
+                        }
+                        break;
+
+                    case 's':
+                        if (numLeft >= 3) {
+                            std::string slice = line.substr(i, 5);
+                            std::string slice2 = line.substr(i, 3);
+                            if (slice == "seven") list.push_back(7);
+                            if (slice2 == "six") list.push_back(6);
+                        }
+                        break;
+
+                    case 'e':
+                        if (numLeft >= 4) {
+                            std::string slice = line.substr(i, 5);
+                            if (slice == "eight") list.push_back(8);
+                        }
+                        break;
+
+                    case 'n':
+                        if (numLeft >= 3) {
+                            std::string slice = line.substr(i, 4);
+                            if (slice == "nine") list.push_back(9);
+                        }
+                        break;
+
+                    default:
+                        continue;
                 }
             }
         }
+
+        // add the calibration value of the line to the sum of all calibration values
+        sum += (list.front() * 10) + list.back();
     }
 
+    // housekeeping
     inputFile.close();
     std::cout << "Day 01 Part 02: " << sum << std::endl;
 }
